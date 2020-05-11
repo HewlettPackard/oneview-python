6a58bee49fd3b4d28149d0176da8b05dde18e6f3
Chebrolu Harika <chebroluharika@gmail.com>
2020-05-11 17:16:01 +0530

    Fix in UTs

--- hpOneView/resources/activity/tasks.py
+++ hpOneView/resources/activity/tasks.py
@@ -36,7 +36,7 @@ class Tasks(Resource):
     URI = '/rest/tasks'
 
     def __init__(self, con):
-        super(Tasks, self).__init__(con, data)
+        super(Tasks, self).__init__(con)
 
     def get(self, id_or_uri):
         """
