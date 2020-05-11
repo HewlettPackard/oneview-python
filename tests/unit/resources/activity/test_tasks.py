816a367a916e515d50047e5080a1d7efa0c19143
Chebrolu Harika <chebroluharika@gmail.com>
2020-05-11 17:05:56 +0530

    Fix in UTs

--- tests/unit/resources/activity/test_tasks.py
+++ tests/unit/resources/activity/test_tasks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###
-# (C) Copyright [2019] Hewlett Packard Enterprise Development LP
+# (C) Copyright [2020] Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,18 +21,18 @@ import mock
 
 from hpOneView.connection import connection
 from hpOneView.resources.activity.tasks import Tasks
-from hpOneView.resources.resource import ResourceClient
+from hpOneView.resources.resource import Resource
 
 
 class TasksTest(TestCase):
     def setUp(self):
         self.host = '127.0.0.1'
         self.connection = connection(self.host)
-        self._client = Tasks(self.connection)
+        self._tasks = Tasks(self.connection)
 
-    @mock.patch.object(ResourceClient, 'get_all')
+    @mock.patch.object(Resource, 'get_all')
     def test_get_all(self, mock_get):
-        self._client.get_all(fields='parentTaskUri,owner,name',
+        self._tasks.get_all(fields='parentTaskUri,owner,name',
                              filter="\"taskState='Running'&filter=associatedResource.resourceCatgory='appliance'\"",
                              sort='name:ascending',
                              view='day')
@@ -42,7 +42,7 @@ class TasksTest(TestCase):
                                                 '.resourceCatgory=\'appliance\'"',
                                          query='', sort='name:ascending', start=0, view='day')
 
-    @mock.patch.object(ResourceClient, 'get')
+    @mock.patch.object(Resource, 'get')
     def test_get_specific(self, mock_get):
-        self._client.get('35323930-4936-4450-5531-303153474820')
+        self._tasks.get('35323930-4936-4450-5531-303153474820')
         mock_get.assert_called_once_with('35323930-4936-4450-5531-303153474820')
