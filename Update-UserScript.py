0f7a47dc475507e577ea47b7ab5fde818eb60ecf
VenkateshRavula <venkatesh.ravula1992@gmail.com>
2020-08-13 14:30:56 +0530

    Debrand hpeOneView python SDK

--- Update-UserScript.py
+++ Update-UserScript.py
@@ -73,8 +73,6 @@ def replacement(path, search_pattern, replace_string, search_string):
                     f = open(path, 'w')                                  # open the file with the WRITE option
                     f.write(strg)                                        # write the the changes to the file
                     f.close()                                            # Closing the file
-            except:
-                print("===== {}".format(path))
 
 
 replacement(path, search_pattern1, replace_string1, search_string1)
