55f409f21eee56c46a540a81074fba4287034700
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-09-07 13:20:59 +0530

    fix flake errors

--- examples/logical_interconnects.py
+++ examples/logical_interconnects.py
@@ -95,9 +95,12 @@ ethernet_settings = logical_interconnect.get_ethernet_settings()
 pprint(ethernet_settings)
 
 # Update the Ethernet interconnect settings for the logical interconnect
+# macRefreshInterval attribute is supported only in C7000
 ethernet_settings = logical_interconnect.data['ethernetSettings'].copy()
-ethernet_settings['macRefreshInterval'] = 10  #This attribute is only supported in C7000
-ethernet_settings['stormControlThreshold'] = 15  #This attribute is supported in Synergy
+if api_variant == 'C7000':
+    ethernet_settings['macRefreshInterval'] = 10
+else:
+    ethernet_settings['stormControlThreshold'] = 15
 logical_interconnect_updated = logical_interconnect.update_ethernet_settings(ethernet_settings)
 print("\nUpdated the ethernet settings")
 print(logical_interconnect_updated.data)
