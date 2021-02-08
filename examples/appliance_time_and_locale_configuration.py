da69106d4258021e3ac46d5787f97b7609a7af65
SHANDCRUZ <m-.shanmugam@hpe.com>
2021-02-08 13:30:44 +0530

    modified_py_code

--- examples/appliance_time_and_locale_configuration.py
+++ examples/appliance_time_and_locale_configuration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###
-# (C) Copyright [2019] Hewlett Packard Enterprise Development LP
+# (C) Copyright [2021] Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 from pprint import pprint
 from hpeOneView.oneview_client import OneViewClient
-from config_loader import try_load_from_file
+from testingexamples.config_loader import try_load_from_file
 
 config = {
     "ip": "<oneview_ip>",
@@ -27,38 +27,42 @@ config = {
     }
 }
 
+options = {
+    "locale": "en_US.UTF-8",
+    "timezone": "UTC",
+    "ntpServers" : ["127.0.0.1"],
+}
+
+
 # Try load config from a file (if there is a config file)
 config = try_load_from_file(config)
-
 oneview_client = OneViewClient(config)
+time_and_locales = oneview_client.appliance_time_and_locale_configuration
 
 # Lists the appliance time and locale configuration
-time_and_locale = oneview_client.appliance_time_and_locale_configuration.get()
-print("\n## Got appliance time and locale configurations successfully!")
-pprint(time_and_locale)
+time_and_locale = time_and_locales.get_all()
+if time_and_locale:
+    print("\n## Got appliance time and locale configurations successfully!")
+    pprint(time_and_locale)
+else:
+    # Create a time and locale with the options provided
+    time_and_locale = time_and_locales.create(data=options)
+    print("\nCreated appliance time and locale configurations successfully!")
+    pprint(time_and_locale.data)
 
-# Backup original values
-bkp = {}
-bkp['ntpServers'] = time_and_locale['ntpServers']
-bkp['locale'] = time_and_locale['locale']
-
-# Update NTP servers and locale
+time_and_locale = time_and_locale.data
 # Set to use appliance local time and date server
 time_and_locale['ntpServers'] = ['127.0.0.1']
 # Set locale to Chinese (China) with charset UTF-8
 time_and_locale['locale'] = 'zh_CN.UTF-8'
 # Remove the date and time, we do not want to update it manually
 time_and_locale.pop('dateTime')
-time_and_locale = oneview_client.appliance_time_and_locale_configuration.update(time_and_locale)
+time_and_locale = time_and_locales.create(data=time_and_locale)
 print("\n## Updated appliance time and locale configurations successfully!")
-pprint(time_and_locale)
-
+pprint(time_and_locale.data)
 # Note: Changing the locale will only be fully effective after resetting the appliance
 
 # Revert the changes made
-time_and_locale['ntpServers'] = bkp['ntpServers']
-time_and_locale['locale'] = bkp['locale']
-time_and_locale.pop('dateTime')
-time_and_locale = oneview_client.appliance_time_and_locale_configuration.update(time_and_locale)
+time_and_locale = time_and_locales.create(data=options)
 print("\n## Reverted appliance time and locale configurations successfully!")
-pprint(time_and_locale)
+pprint(time_and_locale.data)
