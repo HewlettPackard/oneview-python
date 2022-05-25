3f59ed9272af34d99bcdaa3810a8f8341ea0b30b
spapinwar <spapinwar@hpe.com>
2022-05-25 14:36:41 +0530

    7.0 release

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='hpeOneView',
-      version='7.1.0',
+      version='7.0.0',
       description='HPE OneView Python Library',
       url='https://github.com/HewlettPackard/oneview-python',
-      download_url="https://github.com/HewlettPackard/oneview-python/tarball/v7.1.0",
+      download_url="https://github.com/HewlettPackard/oneview-python/tarball/v7.0.0",
       author='Hewlett Packard Enterprise Development LP',
       author_email='oneview-pythonsdk@hpe.com',
       license='Apache',
