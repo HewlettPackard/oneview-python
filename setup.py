b0de221aebf010bd9471095b3625d3c2e545ee3e
VenkateshRavula <venkatesh.ravula@hpe.com>
2023-06-05 21:06:21 +0530

    code changes from jenkins

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='hpeOneView',
-      version='8.3.0',
+      version='8.4.0',
       description='HPE OneView Python Library',
       url='https://github.com/HewlettPackard/oneview-python',
-      download_url="https://github.com/HewlettPackard/oneview-python/tarball/v8.3.0",
+      download_url="https://github.com/HewlettPackard/oneview-python/tarball/v8.4.0",
       author='Hewlett Packard Enterprise Development LP',
       author_email='oneview-pythonsdk@hpe.com',
       license='Apache',
